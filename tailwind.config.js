197739f1913449ecb721c359af053eddaaabfe9a
bapontarindonesia <okipetruslaoh@gmail.com>
2024-08-11 19:24:48 +0900

    portfolio tailwind css

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['index.html'],
+  theme: {
+    container: {
+      center: true,
+      padding: '16px',
+  },
+    extend: {
+      colors: {
+        primary: '#ef4444',
+        dark: '#0f172a',
+      },
+    },
+  },
+  plugins: [],
+}
+
+
